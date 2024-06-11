7ed3ca9acd95403b784a84c8b851c0da12f641d4
Daniel Kauss <daniel.kauss.serna@gmail.com>
2024-06-11 12:52:25 +0200

    new packet format

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,15 @@
 #include "HUSKYLENS.h"
 #include "HardwareSerial.h"
 
-
-#define SERVO_MAX 115
-#define SERVO_MIN 80
+// servo
 #define MOTOR_PIN 14
 
-// Connecion with master
+// connections
 HardwareSerial hs(1);
 HUSKYLENS huskylens;
 HardwareSerial camSerial(2); // RX, TX
 
-
-#define greenId 2
-#define redID 1
-
-// origin is TOP LEFT
-int greenX = 0;
-int greenY = 0;
-bool greenInFrame = false;
-
-int redX = 0;
-int redY = 0;
-bool redInFrame = false;
-
-#define BATTERY_REPORT_RATE 50
-unsigned long last_battery_report = 0;
-
+// serial
 enum SerialCommands {
   SerialMotor,
   SerialServo,
@@ -37,13 +20,36 @@ enum SerialCommands {
   SerialBlocks
 };
 
+// camera 
+TaskHandle_t camera_processing;
+
+#define greenId 2
+#define redID 1
+
+int greenX = 0;
+int greenY = 0;
+bool greenInFrame = false;
+int redX = 0;
+int redY = 0;
+bool redInFrame = false;
+
+// battery
+#define BATTERY_REPORT_RATE 50
+unsigned long last_battery_report = 0;
+
+
+
+// servo
+#define SERVO_MAX 115
+#define SERVO_MIN 70
+#define SERVO_MID 93
 Servo servo;
 
+// motor
 float target_speed = 0;
 float current_speed = 0;
 float elapsed_time;
 unsigned long current_time, previous_time;
-
 unsigned long next_motor_write = 0;
 
 
@@ -79,7 +85,6 @@ void sendSerialBlocks(int color, bool inFrame) {
     hs.write(SerialBlocks);
     hs.write(0); 
     if (inFrame) {
-      Serial.printf("Green is in frame: x -> %i, y -> %i\n", greenX, greenY);
       hs.write((uint8_t *)&greenX, sizeof(int));
       hs.write((uint8_t *)&greenY, sizeof(int));
           } else {
@@ -94,14 +99,13 @@ void sendSerialBlocks(int color, bool inFrame) {
     if (inFrame) {
       hs.write((uint8_t *)&redX, sizeof(int));
       hs.write((uint8_t *)&redY, sizeof(int));
-      Serial.printf("Red is in frame: x -> %i, y -> %i\n", redX, redY);
     } else {
       hs.write((uint8_t *)&negativeOne, sizeof(int));
       hs.write((uint8_t *)&negativeOne, sizeof(int));
     }
   }  
 }
-TaskHandle_t camera_processing;
+
 void cameraProcessing(void *pvParameters) {
   for (;;) {
     vTaskDelay(10);
@@ -165,75 +169,40 @@ void setup() {
 
   camSerial.begin(9600, SERIAL_8N1, 19, 23);
   huskylens.begin(camSerial);
-  // while (!huskylens.begin(camSerial))
-  // {
-  //     Serial.println("Begin failed!");
-  //     Serial.println(F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>Serial 9600)"));
-  //     Serial.println(F("2.Please recheck the connection."));
-  //     delay(100);
-  // }
 
   huskylens.writeAlgorithm(ALGORITHM_COLOR_RECOGNITION); //Switch the algorithm to object tracking.
-
-
   xTaskCreatePinnedToCore(cameraProcessing, "camera", 100000, NULL, 10, &camera_processing, 0);
-
 }
 
 
 
 void loop() {
-
-  // if (greenInFrame && redInFrame) {
-  //   if (greenY > redY) {
-  //     if (greenX < 130) {
-  //       servo.write(70); // right turn
-  //     } else {
-  //       servo.write(90);
-  //     }
-  //   } else {
-  //     if (redX > 180) {
-  //       servo.write(110); // left turn
-  //     } else {
-  //       servo.write(90);
-  //     }
-  //   }
-  // }
-  // if (greenInFrame){
-  //   if (greenX < 130) {
-  //     servo.write(70); // right turn
-  //   } else {
-  //     servo.write(90);
-  //   }
-  //   Serial.printf("Green is in frame: x -> %i, y -> %i\n", greenX, greenY);
-  // }
-  // else if (redInFrame){
-  //   if (redX > 180) {
-  //     servo.write(110); // left turn
-  //   } else {
-  //     servo.write(90);
-  //   }
-  //   Serial.printf("Red is in frame: x -> %i, y -> %i\n", redX, redY);
-  // } else {
-  //   servo.write(90);
-  // }
-
   // receive data from master
   if (hs.available()) {
     int header = hs.read();
     switch (header) {
       case SerialMotor:
         {
-          int speed = 0;
-          hs.readBytes((uint8_t *)&speed, sizeof(int));
+          uint8_t buf[4] = { 0 };
+          hs.readBytes(buf, sizeof(buf));
+          int speed = buf[0] |
+            (buf[1] << 8) |
+            (buf[2] << 16) |
+            (buf[3] << 24);
+          
           target_speed = speed / 100.0f;
           break;
         }
       case SerialServo:
         {
-          int angle = 0;
-          hs.readBytes((uint8_t *)&angle, sizeof(int));
-          servo.write(constrain(angle, SERVO_MIN, SERVO_MAX));  // Shouldn't this be executed periodically along motor_speed?
+          uint8_t buf[4] = { 0 };
+          hs.readBytes(buf, sizeof(buf));
+          float angle = 0;
+          memcpy(&angle, buf, sizeof(float));
+          Serial.printf("angle: %f\n", angle);
+          angle = angle * (360.0f / (2.0f * PI)) + SERVO_MID;
+          angle = constrain(angle, SERVO_MIN, SERVO_MAX);
+          servo.write(angle);  // Shouldn't this be executed periodically along motor_speed?
           break;
         }
     }
@@ -243,7 +212,7 @@ void loop() {
   if (millis() >= next_motor_write) {
     // calculate speed
     current_time = millis();
-    elapsed_time = (float)(current_time - previous_time) + 0.00001F;  // calculate elapsed time
+    elapsed_time = (current_time - previous_time) + 0.00001F;  // calculate elapsed time
     previous_time = current_time;
 
     current_speed = encoder_diff / elapsed_time;
